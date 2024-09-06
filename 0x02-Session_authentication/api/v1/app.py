237914cbb75aa1722382fe806f6efa2731c73e99
markEddy001 <Markeddy254@gmail.com>
2024-09-06 02:45:10 +0000

    Updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,31 +4,32 @@
 import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
+from flask_cors import (CORS, cross_origin)
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
-elif auth_type == 'basic_auth':
+if auth_type == 'basic_auth':
     auth = BasicAuth()
-elif auth_type == 'session_auth':
+if auth_type == 'session_auth':
     auth = SessionAuth()
-elif auth_type == 'session_exp_auth':
+if auth_type == 'session_exp_auth':
     auth = SessionExpAuth()
-elif auth_type == 'session_db_auth':
+if auth_type == 'session_db_auth':
     auth = SessionDBAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -36,17 +37,20 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def authenticate_user():
@@ -60,13 +64,14 @@ def authenticate_user():
             "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
             if auth.authorization_header(request) is None and \
                     auth.session_cookie(request) is None:
                 abort(401)
-            user = auth.current_user(request)
             if user is None:
                 abort(403)
             request.current_user = user
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
