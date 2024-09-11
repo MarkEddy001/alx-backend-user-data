d16369c5b6a3c0d780a0196e85b7d14b6df52f72
Wanyoike Edwards <57659295+MarkEddy001@users.noreply.github.com>
2024-09-11 11:29:53 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,18 +86,15 @@ def profile() -> str:
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
-    """get_reset_password
-
-    Return:
-       str: message
-    """
+    """Generate a reset password token for a user."""
     email = request.form.get('email')
-    user = AUTH.create_session(email)
-    if not user:
+    if email is None:
         abort(403)
-    else:
-        token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": f"{email}", "reset_token": f"{token}"})
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
